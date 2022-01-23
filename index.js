a69861a7072aae75ec735c3a6589bec9c8bebc1c
developersprayer <73502416+developersprayer@users.noreply.github.com>
2022-01-24 03:14:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementById("birthForetold");
+    if (x.style.display === "block") {
+      x.style.display = "none"; 
+    } else {
+      x.style.display = "block"; 
+    }
+    
+  }
+
+  function myFunction2() {
+    var y = document.getElementById("INTRODUCTIONTOMINISTRY");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block"; 
+    }
+    
+  }
